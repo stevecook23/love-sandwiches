9596ce6f331aeec8f4d89765b7a52773818bbc1b
Steve Cook <stevecook23@gmail.com>
2024-06-22 13:32:12 +0100

    Modified data so that it is always an integer

--- run.py
+++ run.py
@@ -19,7 +19,7 @@ def validate_data(values):
     or if there aren't exactly 6 values.
     """
     try:
-        [int(value) for value in values]
+        values = [int(value) for value in values]
         if len(values) != 6:
             raise ValueError(
                 f"Exactly 6 values required, you provided {len(values)}"
@@ -34,7 +34,6 @@ def get_sales_data():
     """
     Get sales figures input from the user
     """
-    
     while True:
         print("Please enter sales data from the last market.")
         print("Data should be six numbers, separated by commas.")
@@ -43,11 +42,22 @@ def get_sales_data():
         data_str = input("Enter your data here: ")
 
         sales_data = data_str.split(",")
-        validate_data(sales_data)
         
         if validate_data(sales_data):
             print("Data is valid!")
-            break
+            return [int(value) for value in sales_data]
+        else:
+            print("Data is invalid. Please try again.")
 
+def update_sales_worksheet(data):
+    """
+    Update sales worksheet, add new row with the list data provided
+    """
+    print("Updating sales worksheet...\n")
+    sales_worksheet = SHEET.worksheet("sales")
+    sales_worksheet.append_row(data)
+    print("Sales worksheet updated successfully.\n")
 
-get_sales_data()
+data = get_sales_data()
+sales_data = [int(num) for num in data]
+update_sales_worksheet(sales_data)
